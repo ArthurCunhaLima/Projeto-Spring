28c5693af8c2d5b10f35963c0b3bf05e11ec68a0
Arthur-Cunhax <arthurcl634@gmail.com>
2025-03-25 16:23:49 -0300

    0.2
    .

--- projeto/src/main/java/com/projeto/ProjetoApplication.java
+++ projeto/src/main/java/com/projeto/ProjetoApplication.java
@@ -2,7 +2,7 @@ package com.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Sistema de Controle de Estoque
+//Sistema de Provas Online, irei fazer um sistema onde o usuário vai escolher a prova objetiva já pré-estabelecida no sistema e poder fazer tendo o retorno com feedback.
 @SpringBootApplication
 public class ProjetoApplication {
 
