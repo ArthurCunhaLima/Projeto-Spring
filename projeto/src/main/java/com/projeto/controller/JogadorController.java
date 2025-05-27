31302752c422e776ce6a2e08b77a53017baa6bc4
HENRIQUE CARVALHO FERNANDES <henriquecafer@gmail.com>
2025-05-26 23:58:51 -0300

    Update JogadorController.java

--- projeto/src/main/java/com/projeto/controller/JogadorController.java
+++ projeto/src/main/java/com/projeto/controller/JogadorController.java
@@ -1,9 +1,6 @@
 package com.projeto.controller;
 
-
-
 import com.projeto.service.JogadorService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +14,12 @@ public class JogadorController {
     public JogadorController(JogadorService jogadorService) {
         this.jogadorService = jogadorService;
     }
+
     @GetMapping("retornarHP")
-    public int retornarHP(){
+    public int retornarHP() {
+        if (jogadorService.getJogador() == null) {
+            return 0; // Retorna 0 se não houver jogador atual
+        }
         return jogadorService.getJogadorHP();
     }
-
-
 }
