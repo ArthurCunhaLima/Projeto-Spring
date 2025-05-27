1538ef1d5940b71b5d0be909cd63e77c8d55cd32
HENRIQUE CARVALHO FERNANDES <henriquecafer@gmail.com>
2025-05-26 23:58:55 -0300

    Update BatalhaService.java

--- projeto/src/main/java/com/projeto/service/BatalhaService.java
+++ projeto/src/main/java/com/projeto/service/BatalhaService.java
@@ -13,10 +13,10 @@ public class BatalhaService {
         Random girarDado = new Random();
         int resultado = girarDado.nextInt(1, 11);
         if (resultado == 10) {
-            jogador.setHP(jogador.getHP() - 100);
+            jogador.setHP(Math.max(0, jogador.getHP() - 100));
             return "Monstro realizou um "+ monstro.getNomeUltimate() + "!!!!\n" +  "Jogador Eliminado";
         }else{
-            jogador.setHP(jogador.getHP() - 30);
+            jogador.setHP(Math.max(0, jogador.getHP() - 30));
             if (jogador.getHP() > 0){
                 return "Monstro Atacou!!! \n" + "Jogador perdeu 30 HP com o ataque de " + monstro.getNomeAtaque() + ", agora você está com " + jogador.getHP();
             }else{
@@ -24,15 +24,25 @@ public class BatalhaService {
             }
         }
         }
-    public String jogadorAtaque(Monstro monstro){
+    public String jogadorAtaque(Monstro monstro) {
+        if (monstro == null || monstro.getHP() <= 0) {
+            return "O monstro já foi derrotado!";
+        }
+
         Random girarDado = new Random();
         int resultado = girarDado.nextInt(1, 3);
-        if (resultado == 1){
-            monstro.setHP(monstro.getHP() - 70);
-            return "CRÍTICO!!!!! \nHP do Monstro :" + monstro.getHP();
-        }else{
-        monstro.setHP(monstro.getHP() - 30);
-        return "Ataque Realizado!! \nHP do Monstro :" + monstro.getHP();
+        if (resultado == 1) {
+            monstro.setHP(Math.max(0, monstro.getHP() - 70));
+            if (monstro.getHP() <= 0) {
+                return "CRÍTICO!!!!! \nO monstro foi derrotado!";
+            }
+            return "CRÍTICO!!!!! \nHP do Monstro: " + monstro.getHP();
+        } else {
+            monstro.setHP(Math.max(0, monstro.getHP() - 30));
+            if (monstro.getHP() <= 0) {
+                return "Ataque Realizado!! \nO monstro foi derrotado!";
+            }
+            return "Ataque Realizado!! \nHP do Monstro: " + monstro.getHP();
         }
     }
 
